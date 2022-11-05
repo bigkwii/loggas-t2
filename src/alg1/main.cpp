241a20d8f4c9f54ac8a1c66d5175714ad6382840
bigkwii <alvaromoralest@gmail.com>
2022-11-04 22:31:28 -0300

    getting somewhere

--- src/alg1/main.cpp
+++ src/alg1/main.cpp
@@ -1,140 +1,173 @@
-// RUN THIS
+// dijkstra implementation borrowed from:
+// https://www.geeksforgeeks.org/dijkstras-algorithm-for-adjacency-list-representation-greedy-algo-8/
+// :3
+
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+#include <queue>
 
 using namespace std;
 
-#define MAX_NODES 16384
-#define MAX_EDGES 16777216
-
-// TODO: IMPLEMENTACIÓN DE VERDAD DE LAS LISTAS DE ADYACENCIA
-// https://www.softwaretestinghelp.com/wp-content/qa/uploads/2019/08/7.undirected-graph-and-its-adjacency-list.png
-
-// SOURCE: https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
-int minDistance(int dist[], bool sptSet[], int V) {
+// A structure to represent a
+// node in adjacency list
+struct AdjListNode {
+    int dest;
+    int weight;
+    struct AdjListNode* next;
+};
  
-    // Initialize min value
-    int min = INT_MAX, min_index;
+// A structure to represent
+// an adjacency list
+struct AdjList {
+     
+   // Pointer to head node of list
+   struct AdjListNode *head;
+};
  
-    for (int v = 0; v < V; v++)
-        if (sptSet[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
+// A structure to represent a graph.
+// A graph is an array of adjacency lists.
+// Size of array will be V (number of
+// vertices in graph)
+struct Graph {
+    int V;
+    struct AdjList* array;
+};
  
-    return min_index;
+// A utility function to create
+// a new adjacency list node
+struct AdjListNode* newAdjListNode(
+                   int dest, int weight) {
+    struct AdjListNode* newNode =
+            (struct AdjListNode*)
+      malloc(sizeof(struct AdjListNode));
+    newNode->dest = dest;
+    newNode->weight = weight;
+    newNode->next = NULL;
+    return newNode;
+}
+ 
+// A utility function that creates
+// a graph of V vertices
+struct Graph* createGraph(int V) {
+    struct Graph* graph = (struct Graph*)
+            malloc(sizeof(struct Graph));
+    graph->V = V;
+ 
+    // Create an array of adjacency lists. 
+    // Size of array will be V
+    graph->array = (struct AdjList*)
+       malloc(V * sizeof(struct AdjList));
+ 
+    // Initialize each adjacency list
+    // as empty by making head as NULL
+    for (int i = 0; i < V; ++i)
+        graph->array[i].head = NULL;
+ 
+    return graph;
+}
+ 
+// Adds an edge to an undirected graph
+void addEdge(struct Graph* graph, int src,
+                   int dest, int weight) {
+    // Add an edge from src to dest. 
+    // A new node is added to the adjacency
+    // list of src.  The node is
+    // added at the beginning
+    struct AdjListNode* newNode =
+            newAdjListNode(dest, weight);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
+ 
+    // Since graph is undirected,
+    // add an edge from dest to src also
+    newNode = newAdjListNode(src, weight);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
 }
 
-// A utility function to print the constructed distance
-// array
-// SOURCE: https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
-void printSolution(int dist[], int V) {
-    cout << "Vertex \t Distance from Source" << endl;
-    for (int i = 0; i < V; i++)
-        cout << i << " \t\t\t\t" << dist[i] << endl;
-}
+// !!! DIJKSTRA HERE !!!
+// simple djikstra implementation only using adjacency lists
+void dijkstra(struct Graph* graph, int src) {
+    int V = graph->V; // # of vertices in graph
+    // index = vertex
+    int dist[V]; // dist values for each vertex
+    int prev[V]; // previous vertex in optimal path
+    int aux[V]; // auxillary queue for dijkstra
 
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
-// SOURCE: https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
-void dijkstra(int ** graph, int src, int V) {
-    int dist[V]; // The output array.  dist[i] will hold the
-                 // shortest
-    // distance from src to i
- 
-    bool sptSet[V]; // sptSet[i] will be true if vertex i is
-                    // included in shortest
-    // path tree or shortest distance from src to i is
-    // finalized
- 
-    // Initialize all distances as INFINITE and stpSet[] as
-    // false
-    for (int i = 0; i < V; i++)
-        dist[i] = INT_MAX, sptSet[i] = false;
- 
-    // Distance of source vertex from itself is always 0
-    dist[src] = 0;
- 
-    // Find shortest path for all vertices
-    for (int count = 0; count < V - 1; count++) {
-        // Pick the minimum distance vertex from the set of
-        // vertices not yet processed. u is always equal to
-        // src in the first iteration.
-        int u = minDistance(dist, sptSet, V);
- 
-        // Mark the picked vertex as processed
-        sptSet[u] = true;
- 
-        // Update dist value of the adjacent vertices of the
-        // picked vertex.
-        for (int v = 0; v < V; v++)
- 
-            // Update dist[v] only if is not in sptSet,
-            // there is an edge from u to v, and total
-            // weight of path from src to  v through u is
-            // smaller than current value of dist[v]
-            if (!sptSet[v] && graph[u][v]
-                && dist[u] != INT_MAX
-                && dist[u] + graph[u][v] < dist[v])
-                dist[v] = dist[u] + graph[u][v];
-    }
- 
-    // print the constructed distance array
-    //printSolution(dist, V);
-    
-    // no need to return anything, we only care about the time it takes to run
-
-    cout << "Finished for " << V << " nodes, trust me :3" << endl;
-}
-
-void run(int V, int rand_range) {
-    // allocate
-    int ** graph = (int **) malloc(V * sizeof(int *));
     for (int i = 0; i < V; i++) {
-        graph[i] = (int *) malloc(V * sizeof(int));
+        dist[i] = INT_MAX;
+        prev[i] = -1;
+        aux[i] = 0;
     }
 
-    // fill in
-    for(int node = 0; node < V; node++){
-        for(int edge = 0; edge < V; edge++){
-            if(node == edge){
-                graph[node][edge] = 0;
+    dist[src] = 0;
+
+    bool auxIsEmpty = false;
+
+    while(!auxIsEmpty){
+        int u;
+        // find min idx in dist and save on u
+        int min = INT_MAX;
+        for (int i = 0; i < V; i++) {
+            if (aux[i] == 0 && dist[i] <= min) {
+                min = dist[i];
+                u = i;
             }
-            if(node < edge){
-                graph[node][edge] = rand() % rand_range;
+        }
+        // mark aux[u] as visited
+        aux[u] = -1;
+
+        // for each neighbor v of u still in aux
+        struct AdjListNode* pCrawl = graph->array[u].head;
+        while (pCrawl != NULL) {
+            int v = pCrawl->dest;
+            if (aux[v] == 0) {
+                int alt = dist[u] + pCrawl->weight;
+                if (alt < dist[v]) {
+                    dist[v] = alt;
+                    prev[v] = u;
+                }
             }
-            if(node > edge){ // symmetric
-                graph[node][edge] = graph[edge][node];
+            pCrawl = pCrawl->next;
+        }
+        // check if aux is empty (i.e: if all elements are -1)
+        auxIsEmpty = true;
+        for (int i = 0; i < V; i++) {
+            if (aux[i] != -1) {
+                auxIsEmpty = false;
+                break;
             }
         }
     }
- 
-    // print
-    // cout << "Graph:" << endl;
-    // for(int node = 0; node < V; node++){
-    //     for(int edge = 0; edge < V; edge++){
-    //         cout << graph[node][edge] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    // cout << endl;
-
-    
-    // Function call
-    dijkstra(graph, 0, V);
-
-    // free mem
+    // return dist and prev
     for (int i = 0; i < V; i++) {
-        free(graph[i]);
+        cout << "Vertex " << i << " has distance " << dist[i] << " and previous vertex " << prev[i] << endl;
     }
-    free(graph);
+
 }
 
 int main(){
-    srand(time(NULL));
-
-    int max_nodes = MAX_NODES;
-
-    run(16384, 12);
+    //lets test it
+    int V = 9;
+    struct Graph* graph = createGraph(V);
+    addEdge(graph, 0, 1, 4);
+    addEdge(graph, 0, 7, 8);
+    addEdge(graph, 1, 2, 8);
+    addEdge(graph, 1, 7, 11);
+    addEdge(graph, 2, 3, 7);
+    addEdge(graph, 2, 8, 2);
+    addEdge(graph, 2, 5, 4);
+    addEdge(graph, 3, 4, 9);
+    addEdge(graph, 3, 5, 14);
+    addEdge(graph, 4, 5, 10);
+    addEdge(graph, 5, 6, 2);
+    addEdge(graph, 6, 7, 1);
+    addEdge(graph, 6, 8, 6);
+    addEdge(graph, 7, 8, 7);
+ 
+    dijkstra(graph, 0);
  
     return 0;
 }
